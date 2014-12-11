16806044cfebfa2f1fe9a04bb0a55a4329ab3680
Alpinoid <valentin.avramko@gmail.com>
2014-12-11 17:04:53 +0300

    Новое измерение "16 Номенклатура" - Создан SQL срипт представления [olap].[v_16_Номенклатура] для измерения - Создано измерение [16 Номенклатура] - Добавлено измерения в куб "Продажи"

---
+++ SQL скрипты для OLAP/Измерения/olap.v_16_Номенклатура.sql
@@ -0,0 +1,31 @@
+USE [UASD]
+GO
+
+/****** Object:  View [olap].[v_16_Номенклатура]    Script Date: 11.12.2014 16:49:30 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+ALTER VIEW [olap].[v_16_Номенклатура]
+AS
+
+SELECT
+	CONVERT(varchar(32), Element._IDRRef, 2) AS ID								-- ID номенклатуры
+	,Element._Fld221 AS Article													-- Артикул
+	,Element._Description AS Description										-- Наименование
+	,Element._Fld221 + ': ' + Element._Description AS ArticleDescription		-- Артикул: Наименовнаие
+	,Element._Description + ' (' + Element._Fld221 + ')' AS DescriptionArticle	-- Наименовнаие (Артикул)
+	,ISNULL(Bussiness._Description, 'Без направления') AS Business				-- Направление бизнеса
+FROM dbo._Reference55 AS Element WITH(NOLOCK)													-- Справочник.Номенклатура
+LEFT JOIN dbo._Reference54 AS Bussiness WITH(NOLOCK) ON Bussiness._IDRRef = Element._Fld165RRef	-- Справочник.НаправленияБизнеса
+WHERE Element._Folder = 0x01
+
+GO
+
+
